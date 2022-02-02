3632e0699a8640086cd8542f4a0071d501970b28
sachingosai <sachingosai.flexion@gmail.com>
2022-02-02 14:42:33 +0530

    - remove whitespace

--- src/middleware/rolePermission.js
+++ src/middleware/rolePermission.js
@@ -3,8 +3,6 @@ const catchError = require('../middleware/catchAsyncerr');
 const rolePermission = catchError(async (req, res, next) => {
     const { token } = await req.headers;
     const id = req.user;
-
-
     if (!token) {
         return res.status(400).json({
             success: false,
@@ -20,7 +18,6 @@ const rolePermission = catchError(async (req, res, next) => {
     }
     const Decode = verify(originalToken, process.env.SECRET);
     const user = await users.findById(Decode.id).select("+userRole");
-
     if (!(user.userRole === 0)) {
         return res.status(403).json({
             success: false,
@@ -33,7 +30,6 @@ const rolePermission = catchError(async (req, res, next) => {
             message: "You dont have permissions for apply changes on your self"
         })
     }
-
     next();
 });
 
