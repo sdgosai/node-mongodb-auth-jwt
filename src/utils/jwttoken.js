34f20aaabfca1709220d4ce176b35b6399e5a8e3
sachingosai <sachingosai.flexion@gmail.com>
2022-02-03 16:15:42 +0530

    - bug fix in token code

--- src/utils/jwttoken.js
+++ src/utils/jwttoken.js
@@ -1,17 +1,12 @@
 const tokenSender = async (user, statusCode, res) => {
     const token = await user.getJWTtoken();
-    /// Options for cookie and expires ...
-    console.log(token);
-    const options = {
-        expires: new Date(
-            Date.now() + 3 * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true
-    };
-    return res.status(statusCode).cookie('token', token, options).json({
+    return res.status(statusCode).send({
         success: true,
-        user
+        message: 'Login Successfull',
+        user,
+        token: 'Bearer ' + token
     })
+
 }
 
 module.exports = { tokenSender };
