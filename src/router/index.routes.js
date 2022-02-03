9ac170323a5b88e7fd5d2e941df04240eab0e622
sachingosai <sachingosai.flexion@gmail.com>
2022-02-03 16:14:37 +0530

    - some routes & require files add

--- src/router/index.routes.js
+++ src/router/index.routes.js
@@ -6,11 +6,23 @@ const Routes = exp.Router();
 const { registrationControll, LoginControll } = require('../controller/userController');
 const { userAuthentication } = require('../middleware/userAuth');
 const rolePermission = require('../middleware/rolePermission');
+const { createSubs, listOfSubs, loginSubs, publicList } = require('../controller/subController');
 
 // Register API
 Routes.post('/register', registrationControll);
 // Login API ...
 Routes.post('/login', LoginControll);
 
+// Filter API ... for subscriber data access by only admin
+Routes.route('/subs/search').post(userAuthentication, rolePermission, listOfSubs).get(userAuthentication, rolePermission, listOfSubs);
+
+/// create subscriber db with admin pannel
+Routes.post('/subs/create', userAuthentication, rolePermission, createSubs);
+
+/// subscriber login
+Routes.post('/subs/login', loginSubs);
+
+/// public list of subscriber access by only logged in users
+Routes.get('/subs/list', userAuthentication, publicList);
 // Router export in server file (index.js) ...
 module.exports = Routes;
