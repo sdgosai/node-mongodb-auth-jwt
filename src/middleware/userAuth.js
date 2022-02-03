dec3917613611df0cb119935845a82b452a54faa
sachingosai <sachingosai.flexion@gmail.com>
2022-02-03 16:12:50 +0530

    - some bug fix in  login errors

--- src/middleware/userAuth.js
+++ src/middleware/userAuth.js
@@ -1,17 +1,45 @@
 const asyncErr = require('./catchAsyncerr');
 const { verify } = require('jsonwebtoken');
 const { User } = require('../models/userModel');
+const { subs } = require('../models/subscribersModel');
 
 const userAuthentication = asyncErr(async (req, res, next) => {
-    const { token } = await req.cookies;
+    const { token } = await req.headers;
     if (!token) {
         res.send({
             success: false,
             message: 'Please login to continue'
         })
     }
-    const Decode = verify(token, process.env.SECRET);
-    req.user = await User.findById(Decode.id);
+    const originalToken = token.substring(7);
+    if (!originalToken) {
+        res.send({
+            success: false,
+            message: 'Please login to continue'
+        })
+    }
+    const Decode = verify(originalToken, process.env.SECRET);
+    User.findById(Decode.id).then(admin => {
+        if (admin) {
+            return req.user = admin
+        } else {
+            subs.findById(Decode.id)
+                .then(sub => {
+                    if (sub) {
+                        return req.user = sub
+                    } else {
+                        return res.send({
+                            success: false,
+                            message: 'Please login to continue'
+                        })
+                    }
+                }).catch(err => {
+                    console.log(err);
+                })
+        }
+    }).catch(err => {
+        console.log(err);
+    })
     next();
 });
 
