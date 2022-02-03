96bf45937aa3f3baee6dd83889b5dac169876bf6
sachingosai <sachingosai.flexion@gmail.com>
2022-02-03 16:15:12 +0530

    - bug fix in removeFileds

--- src/utils/features.js
+++ src/utils/features.js
@@ -5,28 +5,17 @@ class ApiFeature {
     }
     search() {
         const keyword = this.queryStr.keyword ? {
-            // name: {
-            //     $RegExp: this.queryStr.keyword,
-            //     $options: 'i',
-            // }
-            /// if not work ↑ try ↓
             name: new RegExp(this.queryStr.keyword, 'i')
         } : {}
-        // console.log(keyword);
         this.query = this.query.find({ ...keyword });
         return this;
     }
     filter() {
         const queryCopy = { ...this.queryStr };
-        const removeFields = ["keyword", "page", "pages", "limit", "show"];
-        // console.log(queryCopy);
+        const removeFields = ["keyword", "page", "pages", "limit"];
         removeFields.forEach(key => delete queryCopy[key]);
-        // console.log(queryCopy);
-        // filter for age and marks
-        // console.log(queryCopy);
         let queryStr = JSON.stringify(queryCopy);
         queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, key => `$${key}`);
-        // console.log(queryStr);
         this.query = this.query.find(JSON.parse(queryStr));
         return this;
     }
